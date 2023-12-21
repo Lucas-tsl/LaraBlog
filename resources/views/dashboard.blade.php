e9d233e1fe29c06f6dd992375055bcabdd1f1838
Lucas Troteseil <troteseil.lucas@gmail.com>
2023-12-21 14:43:11 +0100

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,35 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    
+    
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+                <!-- Articles -->
+                @foreach ($articles as $article)
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-4">
+                        <div class="p-6 text-gray-900">
+                            <h2 class="text-2xl font-bold">{{ $article->title }}</h2>
+                            <p class="text-gray-700">{{ substr($article->content, 0, 30) }}...</p>
+                            <div class="text-right">
+                                <a href="{{ route('articles.edit', $article->id) }}" class="text-red-500 hover:text-red-700">Modifier</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+        </div>
+
+    </div>
+
+    
+</x-app-layout>
+
